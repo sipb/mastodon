d062ae0710335b8dffc932aa64eead54436598cc
Sam Dukhovni <dukhovni@mit.edu>
2017-05-01 22:29:27 -0400

    Prevent kerberos-squatting

--- app/models/account.rb
+++ app/models/account.rb
@@ -82,6 +82,7 @@ class Account < ApplicationRecord
   validates :display_name, length: { maximum: 30 }, if: -> { local? && will_save_change_to_display_name? }
   validates :note, note_length: { maximum: 500 }, if: -> { local? && will_save_change_to_note? }
   validates :fields, length: { maximum: 4 }, if: -> { local? && will_save_change_to_fields? }
+  validates_with MitValidator, if: -> { local? && will_save_change_to_username? }
 
   scope :remote, -> { where.not(domain: nil) }
   scope :local, -> { where(domain: nil) }
