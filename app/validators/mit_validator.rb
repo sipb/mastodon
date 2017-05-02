7d2b453d36bd0b7eff5400aefdce548ac6d1bb5f
Sam Dukhovni <dukhovni@mit.edu>
2017-05-01 22:29:27 -0400

    Prevent kerberos-squatting

---
+++ app/validators/mit_validator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class MitValidator < ActiveModel::Validator
+  def validate(account)
+    return if !account.local? || account.user.confirmed?
+    account.errors.add(:username, 'Because this username has 8 characters or fewer, it can only be registered with the email address %s@mit.edu.' % account.username) if account.username.length <= 8 && !account.user.email.eql?([account.username, '@mit.edu'].join)
+  end
+end
