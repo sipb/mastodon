a6b17551303488cb9c2b9ca6dc5f31d5c623b7a3
Sam Dukhovni <dukhovni@duxovni.org>
2017-09-19 17:44:52 -0400

    Change IP cleanup time to 7 days

--- app/workers/scheduler/ip_cleanup_scheduler.rb
+++ app/workers/scheduler/ip_cleanup_scheduler.rb
@@ -5,7 +5,7 @@ class Scheduler::IpCleanupScheduler
   include Sidekiq::Worker
 
   def perform
-    time_ago = 5.years.ago
+    time_ago = 7.days.ago
     SessionActivation.where('updated_at < ?', time_ago).destroy_all
     User.where('last_sign_in_at < ?', time_ago).update_all(last_sign_in_ip: nil)
   end
