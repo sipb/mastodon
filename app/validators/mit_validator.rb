0eb4e6e709f4c0d3cbd0c1dfe94a722f5f42f5ea
Tim Hill <timhill@alum.mit.edu>
2018-04-19 00:47:52 -0400

    Allow alumni to register an account with their Kerberos handle

--- app/validators/mit_validator.rb
+++ app/validators/mit_validator.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class MitValidator < ActiveModel::Validator
+  def kerberos_conflict?(username, email)
+    return false if username.length > 8
+    (email != username + '@mit.edu') && (email != username + '@alum.mit.edu')
+  end
+
   def validate(account)
     return if !account.local? || account.user.confirmed?
-    account.errors.add(:username, 'Because this username has 8 characters or fewer, it can only be registered with the email address %s@mit.edu.' % account.username) if account.username.length <= 8 && !account.user.email.eql?([account.username, '@mit.edu'].join)
+    if kerberos_conflict?(account.username, account.user.email)
+      account.errors.add(:username, "Because this username has 8 characters or fewer, it can only be registered with the email address #{account.username}@mit.edu.")
+    end
   end
 end
