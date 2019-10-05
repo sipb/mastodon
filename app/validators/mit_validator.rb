336f6456ff78c752648aefbda294f7273ac3bc13
Max Justicz <max@justi.cz>
2019-10-05 17:26:09 -0400

    Check for nil user in MitValidator

--- app/validators/mit_validator.rb
+++ app/validators/mit_validator.rb
@@ -2,7 +2,7 @@
 
 class MitValidator < ActiveModel::Validator
   def validate(account)
-    return if !account.local? || account.user.confirmed?
+    return if !account.local? || (!account.user.nil? && account.user.confirmed?)
     account.errors.add(:username, 'Because this username has 8 characters or fewer, it can only be registered with the email address %s@mit.edu.' % account.username) if account.username.length <= 8 && !account.user.email.eql?([account.username, '@mit.edu'].join)
   end
 end
